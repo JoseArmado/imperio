75a71473d2d66b95328a77679f38c2fa1a709c5a
JoseArmado <jose_armando_g.v@hotmail.com>
2016-08-18 18:37:29 -0700

    Add files via upload

---
+++ SERVICIO/SERVICIO/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SERVICIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hermosilloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hermosilloToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nacozariToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obregonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obregonToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.navojoaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gaymasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nogalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nogalesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.caborcaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.puertoPesñascoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hermosilloToolStripMenuItem,
+            this.obregonToolStripMenuItem,
+            this.nogalesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hermosilloToolStripMenuItem
+            // 
+            this.hermosilloToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hermosilloToolStripMenuItem1,
+            this.nacozariToolStripMenuItem});
+            this.hermosilloToolStripMenuItem.Name = "hermosilloToolStripMenuItem";
+            this.hermosilloToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.hermosilloToolStripMenuItem.Text = "Hermosillo";
+            // 
+            // hermosilloToolStripMenuItem1
+            // 
+            this.hermosilloToolStripMenuItem1.Name = "hermosilloToolStripMenuItem1";
+            this.hermosilloToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.hermosilloToolStripMenuItem1.Text = "Hermosillo";
+            // 
+            // nacozariToolStripMenuItem
+            // 
+            this.nacozariToolStripMenuItem.Name = "nacozariToolStripMenuItem";
+            this.nacozariToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nacozariToolStripMenuItem.Text = "Nacozari";
+            // 
+            // obregonToolStripMenuItem
+            // 
+            this.obregonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.obregonToolStripMenuItem1,
+            this.navojoaToolStripMenuItem,
+            this.gaymasToolStripMenuItem});
+            this.obregonToolStripMenuItem.Name = "obregonToolStripMenuItem";
+            this.obregonToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.obregonToolStripMenuItem.Text = "Obregon";
+            // 
+            // obregonToolStripMenuItem1
+            // 
+            this.obregonToolStripMenuItem1.Name = "obregonToolStripMenuItem1";
+            this.obregonToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.obregonToolStripMenuItem1.Text = "Obregon";
+            // 
+            // navojoaToolStripMenuItem
+            // 
+            this.navojoaToolStripMenuItem.Name = "navojoaToolStripMenuItem";
+            this.navojoaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.navojoaToolStripMenuItem.Text = "Navojoa";
+            // 
+            // gaymasToolStripMenuItem
+            // 
+            this.gaymasToolStripMenuItem.Name = "gaymasToolStripMenuItem";
+            this.gaymasToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gaymasToolStripMenuItem.Text = "Gaymas";
+            // 
+            // nogalesToolStripMenuItem
+            // 
+            this.nogalesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nogalesToolStripMenuItem1,
+            this.caborcaToolStripMenuItem,
+            this.puertoPesñascoToolStripMenuItem});
+            this.nogalesToolStripMenuItem.Name = "nogalesToolStripMenuItem";
+            this.nogalesToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.nogalesToolStripMenuItem.Text = "Nogales";
+            // 
+            // nogalesToolStripMenuItem1
+            // 
+            this.nogalesToolStripMenuItem1.Name = "nogalesToolStripMenuItem1";
+            this.nogalesToolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.nogalesToolStripMenuItem1.Text = "Nogales";
+            // 
+            // caborcaToolStripMenuItem
+            // 
+            this.caborcaToolStripMenuItem.Name = "caborcaToolStripMenuItem";
+            this.caborcaToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.caborcaToolStripMenuItem.Text = "Caborca";
+            // 
+            // puertoPesñascoToolStripMenuItem
+            // 
+            this.puertoPesñascoToolStripMenuItem.Name = "puertoPesñascoToolStripMenuItem";
+            this.puertoPesñascoToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.puertoPesñascoToolStripMenuItem.Text = "Puerto Pesñasco";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hermosilloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hermosilloToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem nacozariToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem obregonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem obregonToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem navojoaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gaymasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nogalesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nogalesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem caborcaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem puertoPesñascoToolStripMenuItem;
+
+    }
+}
+
---
+++ SERVICIO/SERVICIO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERVICIO
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //conexion
+            string ds = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:/Servicio-Social/SERVICIO/SERVICIO/baseseguro.mdb";
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(ds));
+        }
+    }
+}
